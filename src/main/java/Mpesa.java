5d1b40c7653d26643704cb5506e94abad2b4d2ce
Moses Nandwa <MNANDWA@safaricom.net>
2017-10-29 08:49:39 +0300

    The first commit

---
+++ src/main/java/Mpesa.java
@@ -0,0 +1,351 @@
+
+import okhttp3.*;
+import org.json.*;
+
+import java.io.IOException;
+import java.util.Base64;
+
+public class Mpesa {
+    String appKey;
+    String appSecret;
+    public Mpesa(String app_key, String app_secret){
+        appKey=app_key;
+        appSecret=app_secret;
+    }
+    public String authenticate() throws IOException {
+        String app_key = appKey/*"GvzjNnYgNJtwgwfLBkZh65VPwfuKvs0V"*/;
+        String app_secret = appSecret;
+        String appKeySecret = app_key + ":" + app_secret;
+        byte[] bytes = appKeySecret.getBytes("ISO-8859-1");
+        String encoded = Base64.getEncoder().encodeToString(bytes);
+
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials")
+                .get()
+                .addHeader("authorization", "Basic "+encoded)
+                .addHeader("cache-control", "no-cache")
+
+                .build();
+
+        Response response = client.newCall(request).execute();
+        JSONObject jsonObject=new JSONObject(response.body().string());
+        System.out.println(jsonObject.getString("access_token"));
+        return jsonObject.getString("access_token");
+    }
+    public String C2BSimulation( String shortCode, String commandID, String amount, String MSISDN, String billRefNumber) throws IOException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("ShortCode", shortCode);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("Amount", amount);
+        jsonObject.put("Msisdn", MSISDN);
+        jsonObject.put("BillRefNumber", billRefNumber);
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        System.out.println(requestJson);
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, requestJson);
+        Request request = new Request.Builder()
+                .url("https://sandbox.safaricom.co.ke/safaricom/c2b/v1/simulate")
+                .post(body)
+                .addHeader("content-type", "application/json")
+                .addHeader("authorization", "Bearer "+authenticate())
+                .addHeader("cache-control", "no-cache")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+        return response.body().toString();
+    }
+
+    public String B2CRequest( String initiatorName, String securityCredential,String commandID, String  amount, String partyA,String partyB, String remarks, String queueTimeOutURL, String resultURL, String occassion) throws IOException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("InitiatorName", initiatorName);
+        jsonObject.put("SecurityCredential", securityCredential);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("Amount", amount);
+        jsonObject.put("PartyA", partyA);
+        jsonObject.put("PartyB", partyB);
+        jsonObject.put("Remarks", remarks);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("ResultURL", resultURL);
+        jsonObject.put("Occassion", occassion);
+
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+
+
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, requestJson);
+        Request request = new Request.Builder()
+                .url( "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest")
+                .post(body)
+                .addHeader("content-type", "application/json")
+                .addHeader("authorization", "Bearer "+authenticate())
+                .addHeader("cache-control", "no-cache")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+        return response.body().toString();
+    }
+
+    public String B2BRequest( String initiatorName, String accountReference,String securityCredential,String commandID, String senderIdentifierType,String receiverIdentifierType,float  amount, String partyA,String partyB, String remarks, String queueTimeOutURL, String resultURL, String occassion) throws IOException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("Initiator", initiatorName);
+        jsonObject.put("SecurityCredential", securityCredential);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("SenderIdentifierType", senderIdentifierType);
+        jsonObject.put("RecieverIdentifierType",receiverIdentifierType);
+        jsonObject.put("Amount", amount);
+        jsonObject.put("PartyA", partyA);
+        jsonObject.put("PartyB", partyB);
+        jsonObject.put("Remarks", remarks);
+        jsonObject.put("AccountReference", accountReference);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("ResultURL", resultURL);
+
+
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        System.out.println(requestJson);
+
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, requestJson);
+        Request request = new Request.Builder()
+                .url("https://sandbox.safaricom.co.ke/safaricom/b2b/v1/paymentrequest")
+                .post(body)
+                .addHeader("content-type", "application/json")
+                .addHeader("authorization", "Bearer "+authenticate())
+                .addHeader("cache-control", "no-cache")
+
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+
+    }
+    public String STKPushSimulation(String businessShortCode, String password, String timestamp,String transactionType, String amount, String phoneNumber, String partyA, String partyB, String callBackURL, String queueTimeOutURL,String accountReference, String transactionDesc) throws IOException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("BusinessShortCode", businessShortCode);
+        jsonObject.put("Password", password);
+        jsonObject.put("Timestamp", timestamp);
+        jsonObject.put("TransactionType", transactionType);
+        jsonObject.put("Amount",amount);
+        jsonObject.put("PhoneNumber", phoneNumber);
+        jsonObject.put("PartyA", partyA);
+        jsonObject.put("PartyB", partyB);
+        jsonObject.put("CallBackURL", callBackURL);
+        jsonObject.put("AccountReference", accountReference);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("TransactionDesc", transactionDesc);
+
+
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+
+        OkHttpClient client = new OkHttpClient();
+        String url="https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, requestJson);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("content-type", "application/json")
+                .addHeader("authorization", "Bearer "+authenticate())
+                .addHeader("cache-control", "no-cache")
+                .build();
+
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+        return response.body().toString();
+    }
+    public String STKPushTransactionStatus( String businessShortCode, String password, String timestamp, String checkoutRequestID) throws IOException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("BusinessShortCode", businessShortCode);
+        jsonObject.put("Password", password);
+        jsonObject.put("Timestamp", timestamp);
+        jsonObject.put("CheckoutRequestID", checkoutRequestID);
+
+
+
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+
+
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, requestJson);
+        Request request = new Request.Builder()
+                .url("https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query")
+                .post(body)
+                .addHeader("authorization", "Bearer "+authenticate())
+                .addHeader("content-type", "application/json")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+        return response.body().toString();
+
+    }
+    public String reversal(String initiator, String securityCredential, String commandID, String transactionID, String amount, String receiverParty, String recieverIdentifierType, String resultURL,String queueTimeOutURL, String remarks, String ocassion) throws IOException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("Initiator", initiator);
+        jsonObject.put("SecurityCredential", securityCredential);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("TransactionID", transactionID);
+        jsonObject.put("Amount",amount);
+        jsonObject.put("ReceiverParty", receiverParty);
+        jsonObject.put("RecieverIdentifierType", recieverIdentifierType);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("ResultURL", resultURL);
+        jsonObject.put("Remarks", remarks);
+        jsonObject.put("Occasion", ocassion);
+
+
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        System.out.println(requestJson);
+
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, requestJson);
+        Request request = new Request.Builder()
+                .url("https://sandbox.safaricom.co.ke/safaricom/reversal/v1/request")
+                .post(body)
+                .addHeader("content-type", "application/json")
+                .addHeader("authorization", "Bearer xNA3e9KhKQ8qkdTxJJo7IDGkpFNV")
+                .addHeader("cache-control", "no-cache")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+        return response.body().string();
+    }
+    public String balanceInquiry(String initiator, String commandID, String securityCredential, String partyA, String identifierType, String remarks, String queueTimeOutURL, String resultURL) throws IOException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("Initiator", initiator);
+        jsonObject.put("SecurityCredential", securityCredential);
+        jsonObject.put("CommandID", commandID);
+        jsonObject.put("PartyA", partyA);
+        jsonObject.put("IdentifierType",identifierType);
+        jsonObject.put("Remarks", remarks);
+        jsonObject.put("QueueTimeOutURL", queueTimeOutURL);
+        jsonObject.put("ResultURL", resultURL);
+
+
+
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        System.out.println(requestJson);
+
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, requestJson);
+        Request request = new Request.Builder()
+                .url("https://sandbox.safaricom.co.ke/safaricom/accountbalance/v1/query")
+                .post(body)
+                .addHeader("content-type", "application/json")
+                .addHeader("authorization", "Bearer fwu89P2Jf6MB1A2VJoouPg0BFHFM")
+                .addHeader("cache-control", "no-cache")
+                .addHeader("postman-token", "2aa448be-7d56-a796-065f-b378ede8b136")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+    public  String registerURL(String shortCode, String responseType, String confirmationURL, String validationURL) throws IOException {
+        JSONArray jsonArray=new JSONArray();
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("ShortCode", shortCode);
+        jsonObject.put("ResponseType", responseType);
+        jsonObject.put("ConfirmationURL", confirmationURL);
+        jsonObject.put("ValidationURL", validationURL);
+
+
+
+
+        jsonArray.put(jsonObject);
+
+        String requestJson=jsonArray.toString().replaceAll("[\\[\\]]","");
+        System.out.println(requestJson);
+
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, requestJson);
+        Request request = new Request.Builder()
+                .url("https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl")
+                .post(body)
+                .addHeader("content-type", "application/json")
+                .addHeader("authorization", "Bearer "+authenticate())
+                .addHeader("cache-control", "no-cache")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+        return response.body().string();
+    }
+    public static void main(String[] args) throws IOException {
+        Mpesa m=new Mpesa("GvzjNnYgNJtwgwfLBkZh65VPwfuKvs0V","oOpJICRVlyrGSAkM");
+        m.authenticate();
+        /*m.C2BSimulation("600576","CustomerPayBillOnline","2","254708374149","hkjhjkhjkh");
+        m.authenticate();*/
+/*
+        m.B2CRequest("testapi","BVeDP3XWGFG+NCQri04jHp6c0rCajO1JAOccQ7Bsu/Mup3Rh2Gd9IHQEE0SeA1oBXAt/VBAL/cJP+VKU9qRF6voqCa0P1XG8pcv5hTZUcBkbbb8Qqvqn28+s/tBvsLXwsB4QaageFDDZgS6b6gbK1p7+UZ/hRYHL8WclTpYBrQGfhqKZxduh0bPWvK4rt+uqR3hdVlO0RdJSkcOVCVp+FxizPSk3nI6LFq14Jj2G0TwuQ4a13J/KVu5eeFG65gzE1NnIVouHKeBPz9b9xvove156aR16uxh4rBq5U6UAKC/kUhaJ0wOLTvb762CioudL87C6xaPVdTF4qcSD6jM4PA==","BusinessPayment","22","600576","254708374149","This","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BConfirmation","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BValidation","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BValidation");
+*/
+/*
+        m.B2BRequest("testapi","his","BVeDP3XWGFG+NCQri04jHp6c0rCajO1JAOccQ7Bsu/Mup3Rh2Gd9IHQEE0SeA1oBXAt/VBAL/cJP+VKU9qRF6voqCa0P1XG8pcv5hTZUcBkbbb8Qqvqn28+s/tBvsLXwsB4QaageFDDZgS6b6gbK1p7+UZ/hRYHL8WclTpYBrQGfhqKZxduh0bPWvK4rt+uqR3hdVlO0RdJSkcOVCVp+FxizPSk3nI6LFq14Jj2G0TwuQ4a13J/KVu5eeFG65gzE1NnIVouHKeBPz9b9xvove156aR16uxh4rBq5U6UAKC/kUhaJ0wOLTvb762CioudL87C6xaPVdTF4qcSD6jM4PA==","BusinessPayBill","1", "4",22,"600576","600000","This","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BConfirmation","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BValidation","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BValidation");
+*/
+/*
+        m.STKPushSimulation("174379","MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTcwODI0MTU1MDU1","20170824155055","CustomerPayBillOnline","1","254724513769","254724513769","174379","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BValidation","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BValidation","sasas","asdasd");
+*/
+/*
+        m.reversal("testapi","BVeDP3XWGFG+NCQri04jHp6c0rCajO1JAOccQ7Bsu/Mup3Rh2Gd9IHQEE0SeA1oBXAt/VBAL/cJP+VKU9qRF6voqCa0P1XG8pcv5hTZUcBkbbb8Qqvqn28+s/tBvsLXwsB4QaageFDDZgS6b6gbK1p7+UZ/hRYHL8WclTpYBrQGfhqKZxduh0bPWvK4rt+uqR3hdVlO0RdJSkcOVCVp+FxizPSk3nI6LFq14Jj2G0TwuQ4a13J/KVu5eeFG65gzE1NnIVouHKeBPz9b9xvove156aR16uxh4rBq5U6UAKC/kUhaJ0wOLTvb762CioudL87C6xaPVdTF4qcSD6jM4PA==","TransactionReversal","2121","2","22","4","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BValidation","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BConfirmation","Remarks","Ocassions");
+*/
+/*
+        m.registerURL("600576","Cancelled","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BValidation","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BValidation");
+*/
+/*
+        System.out.println("Hello World!");
+*/
+/*
+        m.balanceInquiry("testapi","AccountBalance","BVeDP3XWGFG+NCQri04jHp6c0rCajO1JAOccQ7Bsu/Mup3Rh2Gd9IHQEE0SeA1oBXAt/VBAL/cJP+VKU9qRF6voqCa0P1XG8pcv5hTZUcBkbbb8Qqvqn28+s/tBvsLXwsB4QaageFDDZgS6b6gbK1p7+UZ/hRYHL8WclTpYBrQGfhqKZxduh0bPWvK4rt+uqR3hdVlO0RdJSkcOVCVp+FxizPSk3nI6LFq14Jj2G0TwuQ4a13J/KVu5eeFG65gzE1NnIVouHKeBPz9b9xvove156aR16uxh4rBq5U6UAKC/kUhaJ0wOLTvb762CioudL87C6xaPVdTF4qcSD6jM4PA==", "600576","4","These","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BConfirmation","http://obscure-bayou-52273.herokuapp.com/api/Mpesa/C2BConfirmation");
+*/
+/*
+        m.STKPushTransactionStatus("174379","MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTcwODI0MTU1MDU1","20170824155055","ws_CO_27102017101215530");
+*/
+    }
+}
